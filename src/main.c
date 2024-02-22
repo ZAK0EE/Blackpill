101f2628bbb403e994be231b02ebcc68474d4194
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2024-02-22 23:15:06 +0200

    Added test code for GPIO

--- src/main.c
+++ src/main.c
@@ -1,34 +1,43 @@
 #include "MCAL/RCC/RCC.h"
-#include "MCAL/stm32f401.h"
+#include "MCAL/GPIO/GPIO.h"
 #include <stdio.h>
 #include "assertparam.h"
 
+
 void assert_failed(uint8_t* file, uint32_t line)
 {
-    while(1);
+    volatile int x;
+    while(1){x++;}
 }
 
 int main()
 {
-    RCC_selectSystemClock(RCC_SYSTEMCLOCK_HSI);
-    volatile int x;
-    volatile int y;
-    x = y = 0;
-    while(1)
+    GPIO_PinConfigTypeDef Pin0 =
     {
-        x = y;
-        y++;
-        x++;
-        if(x > y)
-        {
-            x++;
-        }
-        else if (y > x)
-        {
-            x++;
-        }
-        y = x + y;
+        .PinMode = GPIO_MODE_OUTPUT_PUSHPULL_PULLDOWN,
+        .PinNumber = GPIO_PIN5,
+        .Port = GPIO_GPIOC,
+        .PinSpeed = GPIO_SPEED_MEDIUM,
+    };
+
+        GPIO_initPin(&Pin0);
+
+    RCC_enableAHB1Peripheral(RCC_AHB1PERIPHERAL_GPIOC);
+    for(int i = GPIO_PIN0; i <= GPIO_PIN15; i++)
+    {
+        Pin0.PinNumber = i;
+        GPIO_initPin(&Pin0);
+        GPIO_setPinValue(Pin0.Port, i, GPIO_PINSTATE_SET);
+
 
     }
-    return 0;
+
+
+    int x = 0;
+      while (1)
+    {
+        GPIO_setPinValue(GPIO_GPIOA, GPIO_PIN0, GPIO_PINSTATE_SET);
+
+        GPIO_setPinValue(GPIO_GPIOA, GPIO_PIN0, GPIO_PINSTATE_RESET);
+    }  return 0;
 }
