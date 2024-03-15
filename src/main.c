5065e28571e6b7bbc2a2156607cb2a6b839a56aa
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2024-03-15 21:26:32 +0200

    Code test for NVIC

--- src/main.c
+++ src/main.c
@@ -11,31 +11,26 @@ void assert_failed(uint8_t* file, uint32_t line)
     volatile int x;
     while(1){x++;}
 }
+#include "MCAL/NVIC/NVIC.h"
+void EXTI0_IRQHandler()
+{
+    volatile int x;
 
+    NVIC_IsActive_t y;
+    y = NVIC_getActiveStatus(NVIC_IRQ_EXTI0);    
+    while(1){x++;}
+}
 int main()
 {
+    NVIC_IsActive_t volatile x = NVIC_N_ACTIVE;
+    x = NVIC_getActiveStatus(NVIC_IRQ_EXTI0);
+    NVIC_enableIRQ(NVIC_IRQ_EXTI0);
+  //  NVIC_setPendingIRQ(NVIC_IRQ_EXTI0);
+    NVIC_generateSWInterrupt(NVIC_IRQ_EXTI0);
+    // RCC_enableAHB1Peripheral(RCC_AHB1PERIPHERAL_GPIOA);
+    // LED_Init();
+    // Switch_init();
 
 
-    RCC_enableAHB1Peripheral(RCC_AHB1PERIPHERAL_GPIOA);
-    LED_Init();
-    Switch_init();
-
-    volatile int x;
-      while (1)
-    {
-
-        if(Switch_getSwitchState(SwWITCH_LEDTOGGLE) == SWITCH_PRESSED)
-        {
-            LED_setLedState(LED_RED, LED_ON);
-        }
-        else
-        {
-            LED_setLedState(LED_RED, LED_OFF);
-        }
-        for(int i=0;i<100000;i++)
-        {
-            x++;
-        }
-    } 
     return 0;
 }
